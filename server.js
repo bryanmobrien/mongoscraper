58b912d73025beab268f6c4f2549c331c1377cce
Bryan O'Brien <bryan.m.obrien@gmail.com>
2019-10-27 21:03:16 -0500

    server.js deploy to heroku ossues with port

--- server.js
+++ server.js
@@ -12,24 +12,6 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = process.env.PORT || 3000;
-
-// Initialize Express
-var app = express();
-
-// Configure middleware
-
-// Use morgan logger for logging requests
-// app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Make public a static folder
-app.use(express.static("public"));
-
-// Require all models
-var db = require("./models");
-
 // Initialize Express
 var app = express();
 
